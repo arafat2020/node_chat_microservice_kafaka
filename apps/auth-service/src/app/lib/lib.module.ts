bb13272b2bca76a1fe845f007b79c76e16b7ed0f
arafat2020 <arafatmannan2019@gmail.com>
2025-09-25 12:47:47 +0600

    MOD:Minor adjustment

--- apps/auth-service/src/app/lib/lib.module.ts
+++ apps/auth-service/src/app/lib/lib.module.ts
@@ -3,9 +3,11 @@ import { DdbService } from "./db/db.service";
 import { HashService } from "./utils/hash.service";
 import { JwtService } from "./utils/jwt.service";
 import { JwtService as JwtServiceRoot} from '@nestjs/jwt'
+import { KafkaModule } from "./kafka/kafka.module";
 
 @Global()
 @Module({
+    imports:[KafkaModule],
     providers:[DdbService, HashService, JwtService, JwtServiceRoot],
     exports:[DdbService, HashService, JwtService, JwtServiceRoot]
 })
