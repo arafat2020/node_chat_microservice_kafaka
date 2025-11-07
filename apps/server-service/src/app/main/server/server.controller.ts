b1592c697685483099825dfefce22a12e01033d9
arafat2020 <arafatmannan2019@gmail.com>
2025-11-07 14:48:05 +0600

    INIT: Server Controller initiated, all is left is testing

--- apps/server-service/src/app/main/server/server.controller.ts
+++ apps/server-service/src/app/main/server/server.controller.ts
@@ -2,46 +2,70 @@ import { Controller } from '@nestjs/common';
 import { CreateServerService } from './service/create-server.service';
 import { DeleteServerService } from './service/delete-server.service';
 import { MessagePattern } from '@nestjs/microservices';
-import { CreateServerDto, DeleteServerServiceDto } from '@node-chat/shared';
+import { 
+  CreateServerDto, 
+  DeleteServerServiceDto, 
+  GetInvolveServerDto, 
+  GetServerDto, 
+  LeaveServerDto, 
+  UpdateServerDto
+ } from '@node-chat/shared';
+import { UpdateServerService } from './service/update-server.service';
+import { GetServerService } from './service/getServer.service';
+import { GetInvolvedServerService } from './service/get-involved-server.service';
+import { LeaveServerService } from './service/leave-server.service';
 
 @Controller('server')
 export class ServerController {
   constructor(
     private readonly createServerService: CreateServerService,
-    private readonly deleteServerService: DeleteServerService
+    private readonly deleteServerService: DeleteServerService,
+    private readonly updateServerService: UpdateServerService,
+    private readonly getServerService: GetServerService,
+    private readonly getInvolvedServerService: GetInvolvedServerService,
+    private readonly leaveServerService: LeaveServerService,
   ) {}
 
   @MessagePattern('create.server')
   async createServer(data: CreateServerDto) {
-    try {
-      const server = await this.createServerService.createServer(data);
-      return server;
-    } catch (error) {
-      return {
-        data: null,
-        message:
-          typeof error === 'object' && error !== null && 'message' in error
-            ? (error as any).message
-            : 'Sign in failed',
-        success: false,
-      };
-    }
+
+      return this.createServerService.createServer(data);
+      
   }
 
   @MessagePattern('delete.server')
   async deleteServer(data: DeleteServerServiceDto) {
-    try {
-      await this.deleteServerService.deleteServer(data);
-      return { success: true };
-    } catch (error) {
-      return {
-        data: null,
-        message:
-          typeof error === 'object' && error !== null && 'message' in error
-            ? (error as any).message
-            : 'SignUp in failed',
-        success: false,
-      };
-    }
+    
+      return this.deleteServerService.deleteServer(data);
+    
   }
+
+  @MessagePattern('update.server')
+  async updateServer(data: UpdateServerDto) {
+
+      return this.updateServerService.update(data);
+
+  }
+
+  @MessagePattern('get.server')
+  async getServer(data: GetServerDto) {
+
+      return this.getServerService.get(data);
+
+  }
+
+  @MessagePattern('get.involved.server')
+  async getInvolvedServer(data: GetInvolveServerDto) {
+
+      return this.getInvolvedServerService.get(data);
+
+  }
+
+  @MessagePattern('leave.server')
+  async leaveServer(data: LeaveServerDto) {
+
+      return this.leaveServerService.leaveServer(data);
+
+  }
+
 }
